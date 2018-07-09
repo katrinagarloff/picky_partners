f0c765b2629d5639ca146a1d019cd33ad62c5c4d
katrinagarloff <katrinagarloff@gmail.com>
2018-07-09 15:24:30 -0400

    New user & login views

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2018_07_09_180705) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
-    t.string "email"
+    t.string "username"
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
